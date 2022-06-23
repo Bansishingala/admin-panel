6a8f0f19c70b8e9d6f80b99bc79d64e7287df450
Bansishingala <shingalabansi8@gmail.com>
2022-06-23 15:57:54 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router-dom";
+import Layout from "./Componets/Layout/Layout";
+import Medicine from "./Containers/Medicine/Medicine";
+import Patient from "./Containers/Patient/Patient";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout >
+        <Route path={"/medicines"} exact to={Medicine} />
+        <Route path={"/Patient"} exact to={Patient} />
+    
+      </Layout>
+
+
+    </>
   );
 }
 
